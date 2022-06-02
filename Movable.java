584a95428df228695c75e0cce323d85611c70ef0
luonganhtu28122002 <97713584+luonganhtu28122002@users.noreply.github.com>
2022-06-02 14:04:02 +0700

    Delete Movable.java

--- Movable.java
+++
@@ -1,8 +0,0 @@
-package lab4;
-
-public interface Movable {
-    public abstract int moveUp();
-    public abstract int moveDown();
-    public abstract int moveLeft();
-    public abstract int moveRight();
-}
